9f34ae57661d91c711786fe57a178037b1b3b523
ilia19945 <ilia19945@mail.ru>
2022-01-24 15:32:33 +0300

    oauth + reauthorize + creating a google user with license and groups + dev.juneos user

---
+++ funcs.py
@@ -0,0 +1,233 @@
+import json
+import time
+import requests
+import string
+import random
+import settings
+
+
+# functions
+# just receives google application info
+
+def get_app_info(arg):
+    with open('client_secret_675261997418-4pfe4aep6v3l3pl13lii6p8arsd4md3m.apps.googleusercontent.com.json') as data:
+        json_value = json.load(data)['web'][arg]
+        return json_value
+
+
+# returns the actual token
+def get_actual_token(arg):  # ОБРАТИ ВНИМАНИЕ ЧТО ИСПОЛЬЗУЕТСЯ ФАЙЛ tokens2!!! НА БОЮ ПЕРЕКЛЮЧИТЬ НА TOKENS.JSON
+    with open('access_refresh_tokens2.json') as data:
+        # open the file and parse it to the latest json
+        newest_data = data.read().split('\n')[-2]
+        token = json.loads(newest_data)[arg]
+        return token
+
+
+"""make the http request to refresh the token Step 2:
+https://developers.google.com/identity/protocols/oauth2/web-server#redirecting
+scopes explanation https://developers.google.com/identity/protocols/oauth2/scopes#admin-directory"""
+
+
+def get_new_access_token():
+    refresh_access_token_request = 'https://accounts.google.com/o/oauth2/v2/auth?' \
+                                   'scope=https://www.googleapis.com/auth/admin.directory.user' \
+                                   '+https://www.googleapis.com/auth/apps.licensing' \
+                                   '+https://www.googleapis.com/auth/admin.directory.group.member' \
+                                   '+https://www.googleapis.com/auth/admin.directory.orgunit&' \
+                                   'access_type=offline&' \
+                                   'include_granted_scopes=true&' \
+                                   'response_type=code&' \
+                                   f'redirect_uri={get_app_info("redirect_uris")[0]}&' \
+                                   f'client_id={get_app_info("client_id")}'
+    return refresh_access_token_request
+
+
+""" Step 5: Exchange authorization code for refresh and access tokens
+https://developers.google.com/identity/protocols/oauth2/web-server#exchange-authorization-code"""
+
+
+def exchange_auth_code_to_access_refresh_token(code, jira_key):
+    request_row = 'https://oauth2.googleapis.com/token?' \
+                  f'code={code}&' \
+                  f'client_id={get_app_info("client_id")}&' \
+                  f'client_secret={get_app_info("client_secret")}&' \
+                  f'redirect_uri={get_app_info("redirect_uris")[0]}&' \
+                  'grant_type=authorization_code'
+    # print(request_row)
+    response = requests.post(request_row)  # send and receives response
+
+    # print(response.headers)  # receives headers in dict
+    print(response.json())  # receives body in dict
+    refreshed_token = response.json()
+    if refreshed_token.get("error") is None:
+        refreshed_token['datetime'] = str(time.time_ns())[0:10]  # append datetime to dict
+        # print(refreshed_token)  # receives body in dict
+        refreshed_token = json.dumps(refreshed_token)  # convert dict to json
+
+        # print(refreshed_token)
+        # write json to the end of the file
+        file = open('access_refresh_tokens2.json', 'a')
+        file.write(str(refreshed_token) + '\n')
+        file.close()
+        print('Step 5 executed successfully! Auth code has been exchanged to an access token.\n'
+              'Please repeat creating a user account attempt.')
+        send_jira_comment('Current Auth token was irrelevant and has been exchanged to a new token.\n'
+                          'Please repeat creating a user account attempt.\n'
+                          '(Switch the ticket status -> *"In Progress"* -> *"Create user accounts!"*)',
+                          jira_key)
+    else:
+        print(response)
+
+
+# initiates the refreshing token process WHEN WE HAVE REFRESH TOKEN in the latest google response
+# https://developers.google.com/identity/protocols/oauth2/web-server#offline
+def refresh_token_func():
+    refresh_request = f'{get_app_info("token_uri")}?' \
+                      f'client_id={get_app_info("client_id")}' \
+                      f'&client_secret={get_app_info("client_secret")}' \
+                      f'&refresh_token={get_actual_token("refresh_token")}' \
+                      '&grant_type=refresh_token'
+    refreshed_token = requests.post(refresh_request)  # send and receives response
+
+    # print(refreshed_token.headers)  # receives headers in dict
+    # print(refreshed_token.json())  # receives body in dict
+    refreshed_token = refreshed_token.json()
+    refreshed_token['datetime'] = str(time.time_ns())[0:10]  # append datetime to dict
+
+    # print(refreshed_token)  # receives body in dict
+    refreshed_token = json.dumps(refreshed_token)  # convert dict to json
+
+    # print(refreshed_token)
+    # write json to the end of the file
+    file = open('access_refresh_tokens2.json', 'a')
+    file.write(str(refreshed_token) + '\n')
+
+    file.close()
+    print('access_refresh_tokens.json - appended!\nRefresh token was exchanged successfully.')
+
+
+password = ''
+
+
+# https://developers.google.com/admin-sdk/directory/v1/guides/manage-users
+def create_google_user_req(first_name, last_name, suggested_email, organizational_unit):
+    """google response with another status which means that the account is not can be created."""
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    global password
+    password = ''.join(random.choice(characters) for i in range(8))
+    file = open(r'''C:\Users\ilia1\Desktop\June Homes\User Accounts.txt''', 'a', encoding='utf-8')
+
+    print(f"{first_name} {last_name}\nUsername: {suggested_email}\nPassword: {password}\n\n")
+    url = 'https://admin.googleapis.com/admin/directory/v1/users/'
+    headers = {
+        'Authorization': f'Bearer {get_actual_token("access_token")}'
+    }
+    payload = json.dumps({
+        "primaryEmail": f"{suggested_email}",
+        "name": {
+            "givenName": f"{first_name}",
+            "familyName": f"{last_name}",
+            "fullName": f"{first_name} {last_name}"
+        },
+        "password": f"{password}",
+        "isAdmin": False,
+        "isDelegatedAdmin": False,
+        "agreedToTerms": True,
+        "suspended": False,
+        "changePasswordAtNextLogin": False,
+        "ipWhitelisted": False,
+        "includeInGlobalAddressList": True,
+        "orgUnitPath": f"/Root OU/{organizational_unit}"
+    })
+    # print(payload)
+    response = requests.post(url,
+                             headers=headers,
+                             data=payload)
+    if response.status_code < 300:
+        file.write(f"{first_name} {last_name}\nUsername: {suggested_email}\nPassword: {password}\n\n")
+        file.close()
+    elif response.status_code > 500:
+        print("an error on the google side occurred while creating a google user")
+        print(response.text)
+        return response.status_code, response.__dict__
+    else:
+        print("an error occurred while creating a google user")
+        print(response.text)
+    return response.status_code, response.__dict__, password
+
+
+# https://developers.google.com/admin-sdk/licensing/v1/how-tos/using
+def assign_google_license(google_license_id, suggested_email):
+    url = f"https://www.googleapis.com/apps/licensing/v1/product/Google-Apps/sku/{google_license_id}/user"
+    payload = json.dumps({"userId": f"{suggested_email}"})
+    headers = {'Authorization': f'Bearer {get_actual_token("access_token")}',
+               'Content-Type': 'application/json'}
+    assign_license = requests.post(url=url, headers=headers, data=payload)
+    return assign_license.status_code, assign_license.__dict__
+
+
+# https://developers.google.com/admin-sdk/directory/v1/guides/manage-group-members#create_member
+def adding_user_to_google_group(gmail_groups, suggested_email):
+    payload = json.dumps({
+        "email": f"{suggested_email}",
+        "role": "MEMBER"})
+    headers = {'Authorization': f'Bearer {get_actual_token("access_token")}',
+               'Content-Type': 'application/json'}
+    final_str = ''
+    for i in gmail_groups:
+        url = f"https://admin.googleapis.com/admin/directory/v1/groups/{i}/members"
+        # print(url)
+        assign_google_group = requests.post(url=url, headers=headers, data=payload)
+        if assign_google_group.status_code < 300:
+            final_str += f"Gmail group *{str(i)}* assigning finished with status code: *{assign_google_group.status_code}* \n"
+        else:
+            final_str += f"Gmail group *{str(i)}* assigning finished with error code: *{assign_google_group.status_code}*. " \
+                         f"Error: *{assign_google_group.json()['error']['message']}*\n"
+            # print(assign_google_group.status_code)
+        # print(assign_google_group.json())
+    # print(final_row)
+    return final_str
+
+
+def send_jira_comment(message, jira_key):
+    url = f'https://junehomes.atlassian.net/rest/api/2/issue/{jira_key}/comment'
+    # это кринж надо бы откуда-то токен из внешнего файла доставать, но пока пох
+    headers = {
+        'Authorization': settings.jira_api,  # \X.X/
+        'Content-Type': 'application/json'
+    }
+    data = json.dumps(
+        {"body": f"{message}"})
+    jira_notification = requests.post(url=url, headers=headers, data=data)
+
+    return jira_notification.status_code, jira_notification.json()
+
+
+def create_juneos_dev_user(first_name, last_name, suggested_email, personal_phone, password):
+    url = "https://dev.junehomes.net/api/v2/auth/registration/"
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+    payload = json.dumps({
+        "email": suggested_email,
+        "first_name": first_name,
+        "last_name": last_name,
+        "phone": personal_phone,
+        "password": password,
+        "password2": password,
+        "subscribe": True,
+        "is_staff": True
+    })
+
+    juneos_dev_user = requests.post(url=url, headers=headers, data=payload)
+    if juneos_dev_user.status_code < 300:
+        print('user created')
+        print(juneos_dev_user.json()['user'])
+        return juneos_dev_user.json()['token'], juneos_dev_user.json()['user']
+
+    else:  # if error
+        print(juneos_dev_user.json()['errors'])
+        return juneos_dev_user.json()['errors']
---
+++ mainFastApi.py
@@ -0,0 +1,266 @@
+import requests
+import re
+import json
+import time
+# import random
+# import string
+import funcs as f
+from fastapi import Body, FastAPI, HTTPException, Query  # BackgroundTasks
+from typing import Optional
+
+# import settings
+
+app = FastAPI()
+
+jira_key = ''
+
+# triggers
+# @app.get("/")
+# async def simple_get():
+#     return {"message": "Hello World"}
+print("jira_key:" + jira_key)
+if __name__ == 'mainFastApi':
+
+    @app.get("/"
+             # , name="Don't touch!! :P",
+             # description='This method is a technical restriction of oAuth2.0 Google Authorization process.\n Never use it w/o direct purpose'
+             )
+    async def redirect_from_google(code: Optional[str] = Query(None,
+                                                               description='authorization code from google '
+                                                                           'https://developers.google.com/identity/protocols/oauth2/web-serverexchange-authorization-code'),
+                                   error: str = Query(None,
+                                                      description='The error param google send us when the user denies to confirm authorization')
+                                   ):
+        if code and error:
+            raise HTTPException(status_code=400, detail=f"Bad request. Parameters 'code'={code} and 'error'={error} can\'t be at the same query")
+
+        elif code:
+            if len(code) > 60 and len(code) < 85:  # assuming that auth code from Google is about to 73-75 symbols
+                print('Received a request with code from google')
+                file = open('authorization_codes.txt', 'a')
+                file.write(str(time.asctime()) + ";" + code + '\n')  # add auth code to authorization_codes.txt
+                file.close()
+
+                print("Jira key: " + jira_key)
+                f.exchange_auth_code_to_access_refresh_token(code, jira_key)
+                return {"event": "The authorization code has been caught and saved."
+                                 f"Close this page and go back to jira ticket tab, please."
+                        }
+            else:
+                print('seems like a wrong value has been sent in the code parameter: ', code)
+                return "Are you serious, bro? Is that really code from google?"
+        elif error:
+            print('User denied to confirm the permissions!\nMain flow is stopped!')
+            return 'User denied to confirm the permissions! Main flow is stopped!'
+        else:
+            print('Received a random request')
+            return 'Why are you on this page?=.='
+
+    # webhook from jira
+
+    @app.post("/webhook")
+    async def main_flow(
+            body: dict = Body(...)
+    ):
+        global jira_key
+        jira_key = body['issue']['key']
+
+        detect_change_type = body['changelog']['items'][0]['field']
+        detect_action = body['changelog']['items'][0]['toString']
+        print("Change type:", detect_change_type)
+        if detect_change_type == 'status':
+            jira_old_status = body['changelog']['items'][0]['fromString']
+            jira_new_status = body['changelog']['items'][0]['toString']
+            jira_description = re.split(': |\n', body['issue']['fields']['description'])
+            print("Key: " + jira_key)
+            print("Old Status: " + jira_old_status + "\nNew Status: " + jira_new_status)
+
+            # print(jira_description)
+
+            # detect only the specific status change
+            if jira_old_status == "In Progress" and jira_new_status == "Create user accounts!":
+                print("Correct event to create user accounts detected. Perform user creation attempt ...")
+                print("timestamp: " + str(body['timestamp']))
+                print("webhookEvent: " + body['webhookEvent'])
+                print("user: " + body['user']['accountId'])
+
+                organizational_unit = jira_description[jira_description.index('*Organizational unit*') + 1]
+                first_name = jira_description[jira_description.index('*First name*') + 1]
+                last_name = jira_description[jira_description.index('*Last name*') + 1]
+                if organizational_unit == 'Member Success':
+                    last_name = last_name[0]
+                personal_email = jira_description[jira_description.index('*Personal email*') + 1].split("|")[0][
+                                 0:(len(jira_description[jira_description.index('*Personal email*') + 1]))]
+                #  avoid sending email address in "[name@junehomes.com|mailto:name@junehomes.com] - string"
+                if personal_email[0:1] == '[':
+                    personal_email = personal_email[1:]
+                elif personal_email == '':
+                    personal_email = jira_description[jira_description.index('h4. Personal email') + 1].split("|")[0]
+                    if personal_email[0:1] == '[':
+                        personal_email = personal_email[1:]
+
+                suggested_email = jira_description[jira_description.index('*Suggested name@junehomes.com*') + 1].split("|")[0]
+                # avoid sending email address in "[name@junehomes.com|mailto:name@junehomes.com] - string"
+                if suggested_email[0:1] == '[':
+                    suggested_email = suggested_email[1:]
+                elif suggested_email == '':
+                    suggested_email = jira_description[jira_description.index(
+                        '*Suggested name@junehomes.com*') + 1].split("|")[0]
+                    if suggested_email[0:1] == '[':
+                        suggested_email = suggested_email[1:]
+
+                personal_phone = jira_description[jira_description.index('*Personal phone number (with country code)*') + 1]
+
+                gmail_groups = jira_description[jira_description.index('*Gmail - which groups needs access to*') + 1].split(',')
+                # добавить сюда .strip() чтобы обрезать пробелы
+
+                if '' in gmail_groups:
+                    gmail_groups.remove('')
+                if ' ' in gmail_groups:
+                    gmail_groups.remove(' ')
+                if 'team@junehomes.com' or ' team@junehomes.com' not in gmail_groups:
+                    gmail_groups.append('team@junehomes.com')
+
+                access_token = f.get_actual_token('access_token')
+                datetime = f.get_actual_token('datetime')
+                print("Access_token: " + access_token)
+                print("datetime: " + datetime)
+                print("first_name: " + first_name)
+                print("last_name: " + last_name)
+                print("personal_email: " + personal_email)
+                print("suggested_email: " + suggested_email)
+                print("organizational_unit: " + organizational_unit)
+                print("personal_phone:" + personal_phone)
+                print("gmail_groups (list):" + str(gmail_groups))
+                print('Token lifetime:')
+                print(int(str(time.time_ns())[0:10]) - int(datetime))
+
+                if int(str(time.time_ns())[0:10]) - int(datetime) >= 3600:  # token was refreshed more than 1h ago
+                    print('Access token expired! Try to get get_actual_token("refresh_token"):')
+
+                    try:  # look for refresh token in .json file
+                        refresh_token = f.get_actual_token('refresh_token')
+                        print("Refresh_token: " + refresh_token)
+
+                    except KeyError:  # if there is no refresh token -> new app permission should be requested.
+                        print("Refresh token wasn't found.\nNew access token should be requested before moving forward."
+                              "Prompt the user to follow the link and accept app permissions (if asked):")
+                        new_access_token = f.get_new_access_token()
+                        print(new_access_token)
+                        jira_comment_response = f.send_jira_comment(
+                            "*Access token expired!*\nNew access token should be requested before moving forward. "
+                            "Press the ☢ {color:red}*[BIG RED BUTTON|" + new_access_token + "]""*{color} ☢ "
+                            "and accept app permissions (if asked).\n"
+                            "⚠ *REMEMBER: IT'S ONE TIME LINK! SHOULD BE DELETED AFTER REFRESHING* ⚠\n"
+                            "(It's recommended to open in a new browser tab)\n",
+                            jira_key)
+                        if jira_comment_response[0] > 300:
+                            print("Jira comment wasn't added")
+
+                        print("Jira comment was added successfully")
+
+                    else:  # if refresh token was found in .json file
+                        print('Refresh token was provided by google. Refresh token_func() is executed \n')
+                        f.refresh_token_func()
+                        f.send_jira_comment("Current access_token expired. It was automatically refreshed. "
+                                            "Please try to create user again.\n"
+                                            "(Switch the ticket status -> \"In Progress\" -> \" Create user accounts!\")", jira_key)
+
+                else:
+                    print('Access token is actual. Create new user request will be sent.')
+                    url = f"https://admin.googleapis.com/admin/directory/v1/users/{suggested_email}"
+                    payload = {}
+                    headers = {
+                        'Authorization': f'Bearer {f.get_actual_token("access_token")}'
+                    }
+                    # check if the user with this email already exists
+                    check_for_user_existence = requests.get(url=url, headers=headers, data=payload)
+                    if check_for_user_existence.status_code < 300:
+                        """google response with 200 status which means that the account we're trying to create is already exists"""
+                        print(f"The account ({suggested_email}) is already exist!")
+                        f.send_jira_comment(f"The account ({suggested_email}) *is already exist*!\n"
+                                            f"Check suggested email field and try again.", jira_key)
+                    else:
+                        google_user = f.create_google_user_req(first_name, last_name, suggested_email, organizational_unit)
+                        if google_user[0] < 300:  # user created successfully
+                            print(f"(1/3) User {first_name} {last_name} is successfully created!\n"
+                                  f"Username: {suggested_email}\n")
+                            f.send_jira_comment(f"(1/3) User *{first_name} {last_name}* is successfully created!\n"
+                                                f"User Email: *{suggested_email}*\n", jira_key)
+
+                            # proceeding to licence assignment according the department.
+
+                            if organizational_unit == 'Member Success':  # additional if check needs because that's another function
+                                assigned_license = f.assign_google_license('1010020020', suggested_email)
+                                if assigned_license[0] < 300:  # if success
+                                    print("(2/3) Google Workspace Enterprise Plus license, successfully assigned!")
+                                    f.send_jira_comment(
+                                        "(2/3) *Google Workspace Enterprise Plus* license, successfully assigned!", jira_key)
+                                elif assigned_license[0] == 412:
+                                    f.send_jira_comment(f"Not enough licenses. Google error:\n{assigned_license[1]}", jira_key)
+                                else:  # if error
+                                    f.send_jira_comment("An error appeared while assigning google license.\n"
+                                                        f"Error code: {assigned_license[0]}\n"
+                                                        f"Error message: {assigned_license[1]['error']['message']}", jira_key)
+                                    print(assigned_license[0])
+                                    print(assigned_license[1])  # response body
+
+                            # other department
+                            else:
+                                assigned_license = f.assign_google_license('Google-Apps-Unlimited', suggested_email)
+                                if assigned_license[0] < 300:  # if success
+                                    print("(2/3) *G Suite Business* license, successfully assigned!")
+                                    f.send_jira_comment("(2/3) G Suite Business license, successfully assigned!", jira_key)
+                                elif assigned_license[0] == 412:
+                                    f.send_jira_comment(f"Not enough licenses. Google error:\n{assigned_license[1]}", jira_key)
+                                else:  # if error
+                                    f.send_jira_comment("An error appeared while assigning google license.\n"
+                                                        f"Error code: {assigned_license[0]}\n"
+                                                        f"Error message: {assigned_license[1]['error']['message']}", jira_key)
+                                    print(assigned_license[0])
+                                    print(assigned_license[1])  # response body
+
+                            print("gmail_groups to assign:", gmail_groups)
+                            final_row = f.adding_user_to_google_group(gmail_groups, suggested_email)
+                            print(f"(3/3) {final_row}")
+                            f.send_jira_comment(f"(3/3) Assigned google groups:\n"
+                                                f"{final_row}", jira_key)
+
+                            # need to create user in juneos but impossible due to point 4 & 5
+                            # https://www.notion.so/junehomes/Automatic-user-accounts-creation-82fe3380c2f749ef9d1b37a1e22abe7d
+
+                            # Create user in dev.junehomes.com
+                            if organizational_unit == 'IT':
+                                juneos_dev_user = f.create_juneos_dev_user(first_name=first_name,
+                                                                           last_name=last_name,
+                                                                           suggested_email=suggested_email,
+                                                                           personal_phone=personal_phone,
+                                                                           password=f.password
+                                                                           )
+
+                                if juneos_dev_user[0] == 'token':
+                                    f.send_jira_comment("JuneOS dev. user is created.\n"
+                                                        f"Username: {suggested_email}",
+                                                        jira_key=jira_key)
+                                else:
+                                    print('error')
+
+
+
+                            # other services ...........
+                            #
+                            #
+                            #
+
+                        else:  # error creating google user
+                            f.send_jira_comment("An error occurred while creating google user attempt\n"
+                                                f"Error code: {google_user[0]}\n"
+                                                f"Error response: {google_user[1]}", jira_key)
+
+            else:
+                print('Got a status change different from what triggers the user account creation.')
+
+        else:
+            print(f"The field \"{detect_change_type}\" was changed to: \"{detect_action}\". \n"
+                  "Nothing will be done. Awaiting for the other request")
+            pass
