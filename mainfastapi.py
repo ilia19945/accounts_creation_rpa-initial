21e0f0162e57e7238e0c21d1ae07487a24f0128e
ilia19945 <ilia19945@mail.ru>
2022-12-30 20:07:39 +0200

    removed some test data

--- mainfastapi.py
+++ mainfastapi.py
@@ -106,15 +106,7 @@ if __name__ == 'mainfastapi':
             return 'User denied to confirm the permissions! Main flow is stopped!'
         else:
             fl.error('Received a random request')
-            f.send_jira_comment(message='TEST from MAIN file', jira_key="AUS2-25")
-
-            variables = ''
-            for name, value in os.environ.items():
-                variables += "{0}: {1}".format(name, value) + "\n"
-
-
-            # return {"response": 'Why are you on this page?=.=', "test": requests.get(url='https://ya.ru').text}
-            return {"response": variables, "test": os.environ.get("JIRA_API")}
+            return {"response": 'Why are you on this page?=.='}
 
     # didn't make up on how to use it
     # @app.middleware("http")
